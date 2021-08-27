58237712b08ec1242223b5cb6f94cd0733172e21
Novazembla <nova@pro-pro.local>
2021-08-27 18:08:53 +0200

    more changes

--- apps/dapp/next.config.js
+++ apps/dapp/next.config.js
@@ -4,4 +4,20 @@ module.exports = {
   images: {
     domains: ['localhost'], //TODO: add Api
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       // { and: [/\.(js|ts)x?$/] }
+      },
+      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  
 }
