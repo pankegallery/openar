ce8e9435ef0741456e4dbe3e42ee4b45fd0dd764
Novazembla <nova@pro-pro.local>
2021-08-31 15:16:34 +0200

    enable access control for GrpahQL api

--- apps/api/src/nexus-graphql/helpers/authorizeApiUser.ts
+++ apps/api/src/nexus-graphql/helpers/authorizeApiUser.ts
@@ -6,27 +6,27 @@ export const authorizeApiUser = (
   permissions: PermissionName | PermissionName[],
   doingRefresh = false
 ) => {
-  // TODO: enable auth gateway
-  return true;
-
-  // if (doingRefresh && !ctx.tokenInfo.validRefreshTokenProvided)
-  //   throw Error("GQL authorization rejected");
-
-  // if (
-  //   !doingRefresh &&
-  //   !ctx.tokenInfo.validAccessTokenProvided &&
-  //   ctx.tokenInfo.validRefreshTokenProvided
-  // )
-  //   return Error("Authentication failed (maybe refresh)");
-
-  // if (!ctx.appUser) throw Error("GQL authorization rejected");
-
-  // if (
-  //   !(typeof ctx?.appUser?.can === "function" && ctx.appUser.can(permissions))
-  // )
-  //   throw Error("GQL authorization rejected");
-
+  // // TODO: enable auth gateway
   // return true;
+
+  if (doingRefresh && !ctx.tokenInfo.validRefreshTokenProvided)
+    throw Error("GQL authorization rejected");
+
+  if (
+    !doingRefresh &&
+    !ctx.tokenInfo.validAccessTokenProvided &&
+    ctx.tokenInfo.validRefreshTokenProvided
+  )
+    return Error("Authentication failed (maybe refresh)");
+
+  if (!ctx.appUser) throw Error("GQL authorization rejected");
+
+  if (
+    !(typeof ctx?.appUser?.can === "function" && ctx.appUser.can(permissions))
+  )
+    throw Error("GQL authorization rejected");
+
+  return true;
 };
 
 export default authorizeApiUser;
