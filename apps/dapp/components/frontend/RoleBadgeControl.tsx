e4fb920802d2433b6326c99d9916f9b678097ed3
Novazembla <nova@pro-pro.local>
2021-09-09 13:31:23 +0200

    added missing key to role listing of badges

--- apps/dapp/components/frontend/RoleBadgeControl.tsx
+++ apps/dapp/components/frontend/RoleBadgeControl.tsx
@@ -1,8 +1,7 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
-import {ArrowLink} from "~/components/ui";
+import { ArrowLink } from "~/components/ui";
 import Image from "next/image";
-
 
 import {
   Box,
@@ -17,26 +16,18 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
-export const RoleBadgeControl = ({role}: {role: any;}) => {
-
+export const RoleBadgeControl = ({ role }: { role: any }) => {
   const roleDisclosure = useDisclosure();
 
-  console.log("roleDisclosure:", roleDisclosure)
+  console.log("roleDisclosure:", roleDisclosure);
 
   return (
     <Box>
       {/* ------- Link ------- */}
-      <ArrowLink
-        onClick={roleDisclosure.onOpen}
-      >
-        {role.title}
-      </ArrowLink>
+      <ArrowLink onClick={roleDisclosure.onOpen}>{role.title}</ArrowLink>
 
-      <Modal
-        isOpen={roleDisclosure.isOpen}
-        onClose={roleDisclosure.onClose}
-      >
-        <ModalOverlay bg="blackAlpha.800"/>
+      <Modal isOpen={roleDisclosure.isOpen} onClose={roleDisclosure.onClose}>
+        <ModalOverlay bg="blackAlpha.800" />
         <ModalContent
           color="white"
           pt="0"
@@ -48,25 +39,27 @@ export const RoleBadgeControl = ({role}: {role: any;}) => {
           <ModalHeader pb="4">About the {role.slug} role</ModalHeader>
           <ModalCloseButton fontSize="lg" />
           <ModalBody pb="6">
-            <Text color="white">
-              {role.description}
-            </Text>
-            {role.badges.map(badge => (
-              <>
-                <chakra.p textStyle="label" mt="10" mb="4">{role.title} badge 2021</chakra.p>
-                <Box display="block"><Image
-                  layout="responsive"
-                  width="100%"
-                  height="100%"
-
-                  src={`/images/${badge.image}`}
-                  alt={role.title}
-                /></Box>
-                <chakra.p textStyle="label" mt="6">Artist</chakra.p>
+            <Text color="white">{role.description}</Text>
+            {role.badges.map((badge) => (
+              <Box key={role.slug}>
+                <chakra.p textStyle="label" mt="10" mb="4">
+                  {role.title} badge 2021
+                </chakra.p>
+                <Box display="block">
+                  <Image
+                    layout="responsive"
+                    width="100%"
+                    height="100%"
+                    src={`/images/${badge.image}`}
+                    alt={role.title}
+                  />
+                </Box>
+                <chakra.p textStyle="label" mt="6">
+                  Artist
+                </chakra.p>
                 <p>Created by {badge.artist}</p>
-              </>
+              </Box>
             ))}
-
           </ModalBody>
         </ModalContent>
       </Modal>
