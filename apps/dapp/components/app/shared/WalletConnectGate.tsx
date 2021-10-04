b65c440dc68892742fe6f740154e0cf1ce788d8e
Novazembla <nova@pro-pro.local>
2021-10-04 11:44:11 +0200

    improve logging

--- apps/dapp/components/app/shared/WalletConnectGate.tsx
+++ apps/dapp/components/app/shared/WalletConnectGate.tsx
@@ -30,22 +30,20 @@ export const WalletConnectGate = ({
   const stateCrypto = useTypedSelector(({ crypto }) => crypto);
 
   useEffect(() => {
-  console.log(11, stateUser.justConnected, isLoggingIn, stateUser.authenticated, stateCrypto.signatureRequired);
     const preLogin = async () => {
-      console.log(17);
+      console.log("WalletConnectGate() preLogin()");
       await walletLoginPreLogin(account);
     };
-    console.log(12);
     // no account no chain == not logged in 
     // bail early. 
     if (!account || !chainId) return;
-    console.log(13);
+  
     if (!library || !library?.provider) {
-      console.log(14);
       // looks like that the library has not been initalized 
       // but a signature is required
       // better to logout and get the user to reauthenticate
       if (stateCrypto.signatureRequired) {
+        console.log("WalletConnectGate() walletDisconnect()");
         walletDisconnect();
         setIsLoggingIn(false);
         Router.push(appConfig.reauthenticateRedirectUrl);
@@ -59,7 +57,7 @@ export const WalletConnectGate = ({
       chainId !== parseInt(process.env.NEXT_PUBLIC_CHAIN_ID) &&
       Router.asPath.indexOf("/chain") === -1
     ) {
-      console.log(15);
+      console.log("WalletConnectGate() router.replace(/chain)");
       setIsLoggingIn(false);
       Router.replace("/chain");
       return;
@@ -72,7 +70,6 @@ export const WalletConnectGate = ({
       !isLoggingIn &&
       !stateCrypto.signatureRequired 
     ) {
-      console.log(16);
       setIsLoggingIn(true);
       preLogin();
     }
