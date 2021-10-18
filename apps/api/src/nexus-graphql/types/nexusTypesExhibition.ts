23a3eb81ce5558843e968f720863829949e1559d
Novazembla <nova@pro-pro.local>
2021-10-18 21:26:27 +0200

    optimize fake exhibition

--- apps/api/src/nexus-graphql/types/nexusTypesExhibition.ts
+++ apps/api/src/nexus-graphql/types/nexusTypesExhibition.ts
@@ -113,7 +113,7 @@ export const EventQueries = extendType({
               },
               isPublic: true,
               id: {
-                in: [3, 4, 5, 8, 9, 10, 11, 12, 13, 18, 21, 22],
+                in: [3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22],
               },
             },
             {
