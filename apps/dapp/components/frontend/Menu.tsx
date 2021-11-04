c43c00d50de3363196a7fe461354b273019762c7
Novazembla <nova@pro-pro.local>
2021-11-04 11:55:26 +0100

    small bug fix

--- apps/dapp/components/frontend/Menu.tsx
+++ apps/dapp/components/frontend/Menu.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { AnyObject } from "yup/lib/object";
 import { ActiveLink } from "~/components/ui";
 
 
-export const Menu = ({ pages, href, target, rel }: { pages?: any; target?: any; rel?: any; }) => {
+export const Menu = ({ pages, target, rel }: { pages?: any; target?: any; rel?: any; }) => {
   return (
     <>
       {pages &&
-        pages.map((menuItem) => (
+        pages.map((menuItem: AnyObject) => (
           <ActiveLink key={menuItem.slug} href={`${menuItem.url}`} target={menuItem.target} rel={menuItem.rel}>
             {menuItem.label}
           </ActiveLink>
