95d3ca438c2120630a80269901188261701ded31
Novazembla <nova@pro-pro.local>
2022-02-08 23:17:47 +0100

    remove desc on mobiles exhibitions page

--- apps/dapp/components/frontend/ExhibitionSlide.tsx
+++ apps/dapp/components/frontend/ExhibitionSlide.tsx
@@ -81,7 +81,7 @@ export const ExhibitionSlide = ({
         left="0"
         w="100%"
         h="100%"
-        zIndex="200"
+        zIndex="100"
         templateRows={{
           base: "calc(66.66vw * 2) auto",
           t: "calc(50vw + var(--openar-header-height-desktop)) auto",
@@ -150,11 +150,10 @@ export const ExhibitionSlide = ({
           {/* --------- TILE: Arrows  --------- */}
           <Flex
             w={{
-              base: "calc(33.33vw - 2px)",
-              t: "calc(50vw - 2px)",
-              d: "calc(33.33vw - 2px)",
+              base: "calc(33.33vw - 3px)",
+              t: "calc(50vw - 3px)",
+              d: "calc(33.33vw - 3px)",
             }}
-           
             h={{
               base: "66.66vw",
               t: "50vw",
@@ -171,8 +170,14 @@ export const ExhibitionSlide = ({
               <Box
                 onClick={() => scrollToSlide("prev")}
                 mt="5"
-                pt="10"
-                px="10"
+                pt={{
+                  base: "7",
+                  t: "10"
+                }}
+                px={{
+                  base: "7",
+                  t: "10"
+                }}
                 position="absolute"
                 top="0"
                 right="0"
@@ -180,16 +185,30 @@ export const ExhibitionSlide = ({
                 <Arrow cursor="pointer" className="arrow up" />
               </Box>
             )}
-            <Flex h="100%"
-             py="20"
-             px="10"
-            flexDirection="column"
-            justifyContent="center"><b>{exhibition.description}</b></Flex>
+            <Box
+              h="100%"
+              py="20"
+              px="10"
+              flexDirection="column"
+              justifyContent="center"
+              display={{
+                base: "none",
+                t: "flex"
+              }}
+            >
+              <b>{exhibition.description}</b>
+            </Box>
             {!single && (
               <Box
                 onClick={() => scrollToSlide("next")}
-                pb="10"
-                px="10"
+                pb={{
+                  base: "7",
+                  t: "10"
+                }}
+                px={{
+                  base: "7",
+                  t: "10"
+                }}
                 mb="5"
                 position="absolute"
                 bottom="0"
@@ -211,7 +230,7 @@ export const ExhibitionSlide = ({
             d: "33.33vw",
           }}
           ml={{
-            d: "calc((100vw - 100%))"
+            d: "calc((100vw - 100%))",
           }}
           borderRight="1px solid #fff"
           borderLeft={{
